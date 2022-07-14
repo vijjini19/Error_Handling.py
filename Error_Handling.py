f1726470d70107835251cf4df23d33f0ed216f80
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-07-14 21:37:08 +0530

    Add files via upload

---
+++ Error_Handling.py
@@ -0,0 +1,185 @@
+'''part-1'''
+# print("Python")
+# print("Core Python")
+# try:
+#     print(5/0)
+# except:
+#     print("You Have Divided By Zero")
+# print("Advance Python")
+# print("Python Analytics")
+
+'''part-2'''
+# print("Python")
+# print("Core Python")
+# try:
+#     print(10/5)
+# except:
+#     print('Not ok Divided by zero')
+# print("Advance Python")
+# print("Python Analytics")
+
+'''part-3'''
+# print("Python")
+# print("Core Python")
+# a=5
+# try:
+#     print(b)
+# except ZeroDivisionError:
+#     print('Not ok Divided by zero')
+# print("Advance Python")
+# print("Python Analytics")
+
+'''part-4'''
+# print("Python")
+# print("Core Python")
+# a=5
+# try:
+#     print(a/5)
+#     print(b)
+# except ZeroDivisionError:
+#     print("not ok divided by zero")
+# print("Advance Python")
+# print("Python Analytics")
+
+'''part-5'''
+# print("Python")
+# print("Core Python")
+# a=5
+# try:
+#     print(a/5)
+#     print(a/0)
+# except ZeroDivisionError:
+#     print("not ok  divided by zero")
+# print("Advance Python")
+# print("Python Analytics")
+
+'''part-6'''
+# print("Python")
+# print("Core Python")
+# a=5
+# try:
+#     print(a/5)
+#     print(b)
+# except ZeroDivisionError:
+#     print('Not ok divided by zero')
+# except NameError:
+#     print("you have not divided by name")
+# print("Advance Python")
+# print("Python Analytics")
+
+'''part-7'''
+# print("Python")
+# print("Core Python")
+# a=5
+# b='6'
+# try:
+#     print(a/5)
+#     print(a)
+#     print(a/0)
+# except ZeroDivisionError:
+#     print("not ok divided by zero")
+# print("Advance Python")
+# print("Python Analytics")
+
+# '''part-8'''
+# print("Python")
+# print("Core Python")
+# a=5
+# b='6'
+# try:
+#     print(a/5)
+#     print(a/0)
+#     print(a+"Hello")
+# except ZeroDivisionError:
+#     print("not ok divided by zero")
+# print("Advance Python")
+# print("Python Analytics")
+
+'''part-9'''
+# print("Python")
+# print("Core Python")
+# a=5
+# b='6'
+# try:
+#     print(a/5)
+#     print(a/0)
+#     print(a+a)
+# except ZeroDivisionError:
+#     print("not ok divided by zero")
+# print("Advance Python")
+# print("Python Analytics")
+
+'''part-10'''
+# print("Python")
+# print("Core Python")
+# a=5
+# b='6'
+# try:
+#     print(a/5)
+#     print(a/0)
+#     print(b+'Hello')
+# except ZeroDivisionError:
+#     print("not ok  divided by zero")
+# print("Advance Python")
+# print("Python Analytics")
+
+'''part-11'''
+# print("Python")
+# print("Core Python")
+# a=5
+# b='6'
+# try:
+#     print(a/5)
+#     print(a/0)
+#     print(,)
+# except SyntaxError:
+#     print("you have divided by zero")
+# print("Advance Python")
+# print("Python Analytics")
+
+'''part-12'''
+# print("Python")
+# print("Core Python")
+# a=5
+# b='6'
+# try:
+#     print(a/5)
+# except ZeroDivisionError:
+#     print("Not OK Divided by Zero")
+# finally:
+#     print("Its ok next time dont divide by zero")
+# print("Advance Python")
+# print("Python Analytics")
+
+'''part-13'''
+# print("Python")
+# print("Core Python")
+# a=5
+# b='6'
+# try:
+#     print(a/5)
+#     print(a/0)
+# except ZeroDivisionError:
+#     print("not ok  divided by zero")
+# finally:
+#     print("its ok next time dont divide by zero")
+# print("Advance Python")
+# print("Python Analytics")
+
+'''part-14'''
+# print("Python")
+# print("Core Python")
+# a=5
+# b='6'
+# try:
+#     print(a/5)
+#     print(a/10)
+# except ZeroDivisionError:
+#     print("not ok  divided by zero")
+# else:
+#     print("its ok next time dont divide by zero")
+# print("Advance Python")
+# print("Python Analytics")
+
+
+
